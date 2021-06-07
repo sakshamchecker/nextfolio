3e096f38625f7240cb7838dec4535b9d41407c91
Saksham Checker <checkersaksham@yahoo.com>
2021-06-07 12:31:37 +0530

    Saksham

--- editthis.js
+++ editthis.js
@@ -1,29 +1,28 @@
 var site = {
       /* Your name */
-    name : 'Adithya Pai B',
-    job: 'Front-End Developer',
+    name : 'Saksham Checker',
+    job: 'ML-DL Enthusiast',
        /* This decription is used both in meta tag and about page */
-    description: `Hi there! I am Adithya Pai B from Mangalore, India.
-    I am currently pursuing Bachelor of Engineering in Computer Science.
-    I ❤️ to design Websites, UI/UX, and Photography.`,
+    description: `Hi there! I am Saksham Checker from Delhi, India.
+    I am currently pursuing Bachelor of Engineering in Engineering Physics from Delhi Technological University.
+    I ❤️ to work in Data Science and also explore the field of Cyber Security.`,
     /* Change your skills and title accodingly */
-    skills_title: ['Web Development', 'Design','Programming'],
-    skills: [['HTML', 'CSS', 'JS', 'ReactJS','SCSS'], ['Photoshop', 'XD','Figma'],['C','C++','Python3', 'Javascript']],
+    skills_title: ['Data Science', 'Cyber Security','Programming'],
+    skills: [['Machine Learning', 'Deep Learning', 'Data Structures','Networking'], ['MySQL', 'Linux','Packet Tracer'],['C','C++','Python3']],
     skillp:[[100, 80 ,100, 60, 80], [50, 60, 50], [70,80,90,90]],
     social: {
         /* Add your social media links here */
-        github: 'https://github.com/adithyapaib',
-        twitter: 'https://twitter.com/adithyapaib',
-        linkedin: 'https://www.linkedin.com/in/adithyapaib/',
-        mail: 'mailto: email@gmail.com',
-        instagram: 'https://instagram.com'
+        github: 'https://github.com/sakshamchecker',
+        twitter: 'https://twitter.com/saksham_checker',
+        linkedin: 'https://www.linkedin.com/in/sakshamchecker/',
+        mail: 'mailto: checkersaksham2001@gmail.com',
+        instagram: 'https://instagram.com/20_anonymous_11'
     },
     form: 'https://formsubmit.co/email@gmail.com',
     /* DONT FOR GET TO ACTIVATE UR EMAIL AFTER SETTING UP THE FORM TO RECEIVE EMAILS */
     projects: [
-        ["https://github.com/adithyapaib/portfolio-template", 'https://github.com/adithyapaib/portfolio-template'],
-         ['Calculator', 'Watch'],
-        ['Lorem1', 'Loremum1'],
+        ["https://github.com/sakshamchecker/Sudoku-Solver", 'https://github.com/sakshamchecker/AmazonPriceTracker' ],
+         ['TraMoNis', 'FaceMaskDetection'],
         
     ],
    
